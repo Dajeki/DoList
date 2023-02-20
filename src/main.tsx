ca246a5efe21c91f94dd3a36d8a632908d5cfd5e
Zachary A. K <32288262+Dajeki@users.noreply.github.com>
2023-02-19 23:10:55 -0500

    Add PaletteOption module declaration for type dark/light

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { createTheme, ThemeProvider } from "@mui/system";
+import { createTheme, ThemeProvider } from "@mui/material/styles";
 import { purple } from "@mui/material/colors";
 import "./index.css";
 import App from "./App";
