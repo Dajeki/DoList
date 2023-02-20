907f15c186c8399524d2e6446d6e9d8a6b783834
Zachary A. K <32288262+Dajeki@users.noreply.github.com>
2023-02-19 22:26:28 -0500

    Add MUI with a basic purple theme

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,26 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App";
+import { createTheme, ThemeProvider } from "@mui/system";
+import { purple } from "@mui/material/colors";
 import "./index.css";
+import App from "./App";
+
+const darkPurpleTheme = createTheme({
+	palette: {
+		primary: {
+			main: "#000",
+		},
+		secondary: {
+			main: purple[500],
+		},
+		type: "dark",
+	},
+});
 
 ReactDOM.createRoot( document.getElementById( "root" ) as HTMLElement ).render(
 	<React.StrictMode>
-		<App />
+		<ThemeProvider theme={darkPurpleTheme}>
+			<App />
+		</ThemeProvider>
 	</React.StrictMode>,
 );
